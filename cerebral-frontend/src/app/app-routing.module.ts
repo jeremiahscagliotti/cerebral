0405328fcd71aaafb083e55094f20b12cd6b4af4
jeremiahscagliotti <jeremiahscagliotti@gmail.com>
2018-04-26 15:28:23 -0700

    some components add

--- cerebral-frontend/src/app/app-routing.module.ts
+++ cerebral-frontend/src/app/app-routing.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-import { PortalComponent } from './portal/portal.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './profile/profile.component';
+// import { PortalComponent } from './portal/portal.component';
+// import { HeaderComponent } from './header/header.component';
+// import { FooterComponent } from './footer/footer.component';
+// import { SignUpComponent } from './sign-up/sign-up.component';
+// import { LoginComponent } from './login/login.component';
+// import { ProfileComponent } from './profile/profile.component';
 
 
 const routes: Routes = [
-  { path: 'profile/:id', component: ProfileComponent },
-  { path: 'registration', component: SignUpComponent },
+  //{ path: 'profile/:id', component: ProfileComponent },
+  //{ path: 'registration', component: SignUpComponent }
+  //{ path: '', pathMatch: 'full', redirectTo: 'registration' }
 ];
 
 @NgModule({
