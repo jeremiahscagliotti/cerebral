f438087c05c49a529838e053287777ba49b56654
jeremiahscagliotti <jeremiahscagliotti@gmail.com>
2018-04-25 17:51:50 -0700

    routes and forms

--- cerebral-frontend/src/app/app.module.ts
+++ cerebral-frontend/src/app/app.module.ts
@@ -5,20 +5,37 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+//Http
 import { HttpService } from "./http.service";
 import { HttpClientModule } from '@angular/common/http';
 
+//form  
+import { FormsModule } from '@angular/forms';
 
+//  components
+import { PortalComponent } from './portal/portal.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortalComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignUpComponent,
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
--- cerebral-frontend/src/app/http.service.ts
+++ cerebral-frontend/src/app/http.service.ts
@@ -6,4 +6,20 @@ export class HttpService {
 
   constructor( private _http: HttpClient) { }
 
+  NewUser(user) {
+    return this._http.post('/users/', user);
+  }
+  GetUsers() {
+    return this._http.get('/users');
+  }
+  GetSelectedUser(id) {
+    return this._http.get('/users/' + id);
+  }
+  EditeUser(user) {
+    return this._http.put('/users/' + user._id, user);
+  }
+  DeleteUser(id) {
+    return this._http.delete('/users/' + id);
+  }
+
 }
